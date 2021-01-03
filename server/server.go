20345e938a7981f9a5bf8708381b785e3dac7066
照井寛也 <s.3.h.st.sp211@gmail.com>
2021-01-03 15:54:59 +0900

    server.goをhello world を返すように仮実装

--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
+	"strings"
 )
 
 func StartServer() {
@@ -12,4 +15,24 @@ func StartServer() {
 		log.Fatalln(err)
 	}
 	log.Println("webサーバーを開始します")
+
+	//1回でlistenerが閉じてしまわないようにfor文かつgoroutineで回す
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("listenerのacceptでエラーが発生しました")
+			log.Fatalln(err)
+		}
+		go func() {
+			//レスポンスを書き込む
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
+			}
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
 }
