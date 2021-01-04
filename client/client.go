2378a76621e14b305979a694bd5532b0a7c4b75a
Hiroya3 <s.3.h.st.sp211@gmail.com>
2021-01-04 18:55:17 +0900

    Revert "テストコードを書くためにキーワードを構造体に入れるように変更"
    This reverts commit 63c8dc512bb42a7f9f783b08b698696ac417e93d.

--- client/client.go
+++ client/client.go
@@ -3,51 +3,33 @@ package client
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 )
 
-type SearchingWords struct {
-	Stdin    io.Reader
-	Keyword1 string
-	Keyword2 string
-	Keyword3 string
-}
-
-func createSearchingWords() *SearchingWords {
-	return &SearchingWords{
-		Stdin:    os.Stdin,
-		Keyword1: "",
-		Keyword2: "",
-		Keyword3: "",
-	}
-}
+var searchingWords []string
 
 //コマンドラインに案内画面を表示します。
 func ViewCmd() {
-	SearchingWords := createSearchingWords()
 	for {
 		fmt.Println("ファイルを検索します。キーワードをスペース区切りで3つまで指定してください。\n３つ以上指定した場合ははじめ３つが採用されます。")
-		words := getWords()
+		searchingWords = getSearchingWords()
 
-		if len(words) > 3 {
+		if len(searchingWords) > 3 {
 			fmt.Println("4つ以上指定されたため、はじめの３つを取得します。")
+			searchingWords = searchingWords[:3]
 		}
-		if len(words) > 0 {
-			SearchingWords.Keyword1 = words[0]
-			SearchingWords.Keyword2 = words[1]
-			SearchingWords.Keyword3 = words[2]
+		if len(searchingWords) > 0 {
 			break
 		}
-		if len(words) == 0 {
+		if len(searchingWords) == 0 {
 			fmt.Println("キーワードを指定してください。")
 		}
 	}
 }
 
 //検索ワードを3つ取得します。4つ以上入力された場合ははじめの3つが取得されます。
-func getWords() []string {
+func getSearchingWords() []string {
 	var searchingWords = []string{}
 
 	sc := bufio.NewScanner(os.Stdin)
