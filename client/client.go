63c8dc512bb42a7f9f783b08b698696ac417e93d
Hiroya3 <s.3.h.st.sp211@gmail.com>
2021-01-04 18:43:01 +0900

    テストコードを書くためにキーワードを構造体に入れるように変更

--- client/client.go
+++ client/client.go
@@ -3,33 +3,51 @@ package client
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
-var searchingWords []string
+type SearchingWords struct {
+	Stdin    io.Reader
+	Keyword1 string
+	Keyword2 string
+	Keyword3 string
+}
+
+func createSearchingWords() *SearchingWords {
+	return &SearchingWords{
+		Stdin:    os.Stdin,
+		Keyword1: "",
+		Keyword2: "",
+		Keyword3: "",
+	}
+}
 
 //コマンドラインに案内画面を表示します。
 func ViewCmd() {
+	SearchingWords := createSearchingWords()
 	for {
 		fmt.Println("ファイルを検索します。キーワードをスペース区切りで3つまで指定してください。\n３つ以上指定した場合ははじめ３つが採用されます。")
-		searchingWords = getSearchingWords()
+		words := getWords()
 
-		if len(searchingWords) > 3 {
+		if len(words) > 3 {
 			fmt.Println("4つ以上指定されたため、はじめの３つを取得します。")
-			searchingWords = searchingWords[:3]
 		}
-		if len(searchingWords) > 0 {
+		if len(words) > 0 {
+			SearchingWords.Keyword1 = words[0]
+			SearchingWords.Keyword2 = words[1]
+			SearchingWords.Keyword3 = words[2]
 			break
 		}
-		if len(searchingWords) == 0 {
+		if len(words) == 0 {
 			fmt.Println("キーワードを指定してください。")
 		}
 	}
 }
 
 //検索ワードを3つ取得します。4つ以上入力された場合ははじめの3つが取得されます。
-func getSearchingWords() []string {
+func getWords() []string {
 	var searchingWords = []string{}
 
 	sc := bufio.NewScanner(os.Stdin)
