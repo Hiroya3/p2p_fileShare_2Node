56c4d7e9c16e12b361df893ded2245b65697bb96
照井寛也 <s.3.h.st.sp211@gmail.com>
2021-01-10 23:56:08 +0900

    コメントの削除

--- client/export_test.go
+++ client/export_test.go
@@ -4,8 +4,6 @@ import (
 	"io"
 )
 
-//clientパッケージとclient_testパッケージのunexportな変数・メソッドをつなげるファイル
-
 func T_getSearchingWordsBySpace(t_stdin io.Reader) []string {
 	return getSearchingWordsBySpace(t_stdin)
 }
