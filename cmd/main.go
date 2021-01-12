7f5bfb2691538d9d6609ded22f9caded292d7718
照井寛也 <s.3.h.st.sp211@gmail.com>
2021-01-12 11:35:02 +0900

    ノード情報の取得

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"p2p_fileShare_2Node/client"
 	"p2p_fileShare_2Node/server"
 )
+
+type address struct {
+	OwnDNS     string `json:"ownDNS"`
+	OwnPort    int    `json:"ownPort"`
+	TargetDNS  string `json:"targetDNS"`
+	TargetPort int    `json:"targetPort"`
+}
+
+var Address address
+
+//ノードの情報の読み取り
+func init() {
+	// nodeInfo/address.jsonから自ノードと相手ノードの情報を取得し、struct/address.goに格納
+	jsonFile, err := ioutil.ReadFile("../nodeInfo/address.json")
+	if err != nil {
+		log.Fatalln("address.jsonの読み取りに失敗しました。エラー：%s", err)
+	}
+
+	err = json.Unmarshal(jsonFile, &Address)
+
+	if err != nil {
+		log.Fatalln("address.jsonの解析でエラーが発生しました。エラー：%s", err)
+	}
+}
 
 func main() {
 	/*
