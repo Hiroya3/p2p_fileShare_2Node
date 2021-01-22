0caf2e0a2d4c67dda73839a4cd21252384c56972
照井寛也 <s.3.h.st.sp211@gmail.com>
2021-01-22 19:01:56 +0900

    コメントの挿入

--- Node/errorStatus/error.go
+++ Node/errorStatus/error.go
@@ -3,18 +3,19 @@ package errorStatus
 type ErrorCode int
 
 const (
-	ErrCode300 ErrorCode = 300
+	ErrCode300 ErrorCode = 300 //チェックサムが一致せず改竄があったとき
 )
 
 func (e ErrorCode) Error() string {
 	switch e {
 	case ErrCode300:
-		return "error code 300"
+		return "tampering happend because checksum is wrong"
 	default:
 		return "unknown error code"
 	}
 }
 
+//改竄があったときのエラーコードを返す
 func ReturnErrorCode300() error {
 	return ErrCode300
 }
