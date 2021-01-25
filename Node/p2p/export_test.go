85c18efe30d7f4cd7581b1af49ae439b78f4d730
照井寛也 <s.3.h.st.sp211@gmail.com>
2021-01-25 16:37:12 +0900

    命名規則の変更

--- Node/p2p/export_test.go
+++ Node/p2p/export_test.go
@@ -2,11 +2,11 @@ package p2p
 
 import "net"
 
-func T_readRequestMessage(conn net.Conn) ([]string, error) {
+func TreadRequestMessage(conn net.Conn) ([]string, error) {
 	messageSlice, err := readRequestMessage(conn)
 	return messageSlice, err
 }
 
-func T_compareHash(requestBodyStr, requestHash string) bool {
+func TcompareHash(requestBodyStr, requestHash string) bool {
 	return compareHash(requestBodyStr, requestHash)
 }
--- Node/p2p/socket_test.go
+++ Node/p2p/socket_test.go
@@ -11,7 +11,7 @@ func TestCompareHash(t *testing.T) {
 	requestBodyStr := "001:searchWords:aa,bb,cc:"
 	//ハッシュ値
 	hash := "07c53b87cdf1f44b865d88254472a5be50bbdb3de41cb56c2b5f5fb1b50747fc"
-	if !p2p.T_compareHash(requestBodyStr, hash) {
+	if !p2p.TcompareHash(requestBodyStr, hash) {
 		t.Errorf("%sのハッシュ値（%s）の生後判定が誤っています。", requestBodyStr, hash)
 	}
 }
